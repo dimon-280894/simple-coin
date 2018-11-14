6745ce92a1289625b08bea0ce85e47ba893f633f
Dmitrii <dimonk280894@gmail.com>
2018-11-14 09:36:55 +0500

    miners 2

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -72,7 +72,7 @@ class CMainParams : public CChainParams {
 public:
     CMainParams() {
         strNetworkID = "main";
-        consensus.nSubsidyHalvingInterval = 720; // Note: actual number of blocks per calendar year with DGW v3
+        consensus.nSubsidyHalvingInterval = 500; // Note: actual number of blocks per calendar year with DGW v3
         consensus.nMasternodePaymentsStartBlock = 100; // ~5 days.Not true, but it's ok as long as it's less then nMasternodePaymentsIncreaseBlock
         consensus.nMasternodePaymentsIncreaseBlock = 158000; // actual historical value
         consensus.nMasternodePaymentsIncreasePeriod = 576*30; // 17280 - actual historical value
